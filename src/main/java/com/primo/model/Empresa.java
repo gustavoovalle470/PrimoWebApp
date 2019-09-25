ef65478c918558ae740b01a01119fd7984a6d7f9
Gustavo Adolfo Ovalle Quintero <gustavo.ovalle@unisys.com>
2019-09-25 10:43:37 -0500

    Pantalla inicial de registro de empresa.

--- src/main/java/com/primo/model/Empresa.java
+++ src/main/java/com/primo/model/Empresa.java
@@ -10,14 +10,6 @@ import java.io.Serializable;
 import java.math.BigInteger;
 import java.sql.Blob;
 import java.util.Date;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
 
 /**
  * Clase que representa el Objeto Empresa
